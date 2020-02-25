a0c551a93f8a2850b7863a5647cde6b6196a287c
Isabel Piedrahita <ipiedrahiv@eafit.edu.co>
2020-02-25 14:36:06 -0500

    Added Practice and Macroprocess models

--- mm_evaluation/models.py
+++ mm_evaluation/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class SpecificPractice(models.Model):
+    description = models.CharField(max_length = 500)
+    recommendation = models.CharField(max_length = 500)
+    process_id = models.IntegerField()
+    score = models.IntegerField()
+
+    class Meta:
+        db_table = 'specific_practice'
+
+        
+class GeneralPractice(models.Model):
+    description = models.CharField(max_length = 500)
+    recommendation = models.CharField(max_length = 500)
+    score = models.IntegerField()
+    
+    class Meta:
+        db_table = 'general_practice'
+
+        
+class Macroprocess(models.Model):
+    description = models.CharField(max_length = 500)
+    name = models.CharField(max_length = 50)
+
+    class Meto:
+        db_table = 'macroprocess'
