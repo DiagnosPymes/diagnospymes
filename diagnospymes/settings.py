84194df42095791c0b095b5cf720145e3e8905d6
Vincent A. Arcila <jandro.vins@gmail.com>
2020-05-20 09:31:54 -0500

    Added lo fo ALLOWED_HOSTS

--- diagnospymes/settings.py
+++ diagnospymes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['DIAGNOSPYMES_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['diagnospymes.dis.eafit.edu.co', '192.168.10.182']
+ALLOWED_HOSTS = ['diagnospymes.dis.eafit.edu.co', '192.168.10.182', '127.0.0.1']
 
 
 # Application definition
