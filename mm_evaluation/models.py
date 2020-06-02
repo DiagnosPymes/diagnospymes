d7a9948329a17349f67a75d076a8399ccdba55e6
Vincent A. Arcila <jandro.vins@gmail.com>
2020-06-02 14:24:50 -0500

    Update models.py

--- mm_evaluation/models.py
+++ mm_evaluation/models.py
@@ -82,7 +82,6 @@ VALID_INVENTORY_POLITICS = [
     ("CTO","CTO: configurar planta bajo pedido"),
 ]
 """Valid inventory politics for finances information"""
-"""Every PYME has to be related to a Sector."""
 
 class Sector(models.Model):
     """
