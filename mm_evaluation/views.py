667001ea5de31c23dc531396e0b68f49256bae63
marquez8541 <60795344+marquez8541@users.noreply.github.com>
2020-09-15 09:41:07 -0500

    Update views.py

--- mm_evaluation/views.py
+++ mm_evaluation/views.py
@@ -183,12 +183,10 @@ class IndexView(TemplateView):
         context = {
             "title": "¿Quiénes somos?",
             "content": "El SFLMM es un modelo liviano que se enfoca en la evaluación de prácticas recomendadas en procesos mayormente logísticos de las empresa, lo anterior con el propósito de implementar hábitos de mejora continua.",
-        }
-        context = {
+      
             "title_uno": "¿Cuál es nuestra misión?",
             "content_uno": "Apoyar el desarrollo de la competitividad y productividad de las micro y pequeñas organizaciones a través de una evaluación consciente de sus procesos.",
-        }
-        context = {
+      
             "title_dos": "Nuestra misión",
             "content_dos": "Evolucionar como un sistema de autodiagnóstico integrado y personalizado a cada uno de los intereses de las micro y pequeñas organizaciones del Valle de Aburrá.",
         }
